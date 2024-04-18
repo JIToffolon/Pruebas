0f4e5a8354a3e345c479e95f306f111638c60730
Juan Ignacio Toffolon <juanignaciotoffolon@gmail.com>
2024-04-18 20:49:50 -0300

    Tercer commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { HookApp } from './App';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <CounterApp />
+    <CounterWithCustomHook />
  
 );
 
