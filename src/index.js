e6a41dc971b568d345005cf1b7014f5ed8613f98
Juan Ignacio Toffolon <juanignaciotoffolon@gmail.com>
2024-04-18 20:27:18 -0300

    Segundo Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HookApp } from './App';
+// import { HookApp } from './App';
+import { CounterApp } from './components/01-useState/CounterApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <HookApp />
+    <CounterApp />
  
 );
 
